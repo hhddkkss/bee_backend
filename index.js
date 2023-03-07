40044afc804b480c7f2574b46ab10609944f03b6
hhddkkss <116135735+hhddkkss@users.noreply.github.com>
2023-03-07 14:22:43 +0800

    最新

--- index.js
+++ index.js
@@ -24,16 +24,14 @@ const db = require('./modules/mydb-connect')
 //session
 const session = require('express-session')
 //mysql session(higher-order function)
-const MysqlStore = require('express-mysql-session')(session);
+const MysqlStore = require('express-mysql-session')(session)
 //
-const sessionStore = new MysqlStore({},dataBase);
+const sessionStore = new MysqlStore({}, dataBase)
 
 //密碼加密bcrypt
-const bcrypt = require('bcryptjs');
+const bcrypt = require('bcryptjs')
 //Token
 const jwt = require('jsonwebtoken')
-
-
 
 //2.建立 web server 物件
 const app = express()
@@ -43,15 +41,17 @@ app.set('views', __dirname + '/views')
 //設定白名單
 
 //session設定
-app.use(session({
-  saveUninitialized:false,
-  resave:false,
-  secret:'fueihfiwjcf%%xsjw',
-  store: sessionStore,
-  cookie:{
-      maxAge:12000000
-  }
-}));
+app.use(
+  session({
+    saveUninitialized: false,
+    resave: false,
+    secret: 'fueihfiwjcf%%xsjw',
+    store: sessionStore,
+    cookie: {
+      maxAge: 12000000,
+    },
+  })
+)
 //Top-level middleware
 app.use(express.urlencoded({ extended: false }))
 app.use(express.json())
@@ -82,70 +82,55 @@ app.get('/try_db2', async (req, res) => {
   res.json(rows)
 })
 
-
-
 //會員登入登出
-app.get('/login',(req,res)=>{
+app.get('/login', (req, res) => {
   res.send('hi!')
 })
-app.post('/login' ,async(req, res)=>{
+app.post('/login', async (req, res) => {
   let output = {
-    success : false,
-    error:'',
-    postData:req.body,
-    token:'',
+    success: false,
+    error: '',
+    postData: req.body,
+    token: '',
   }
 
-  const sql = "SELECT * FROM `member_list` WHERE `email` = ?";
-  const [rows] = await dataBase.query(sql,[req.body.email]);
+  const sql = 'SELECT * FROM `member_list` WHERE `email` = ?'
+  const [rows] = await dataBase.query(sql, [req.body.email])
   //帳號錯誤判斷
-   if(!rows.length){
-    output.error='Oh!帳號密碼錯誤!'
-    return res.json(output);
-   }
+  if (!rows.length) {
+    output.error = 'Oh!帳號密碼錯誤!'
+    return res.json(output)
+  }
 
-   //密碼驗證
-   let passwordCorrect = false;
-   try{
-    passwordCorrect = await bcrypt.compare(req.body.password,rows[0].password)
-   }catch(ex){}
+  //密碼驗證
+  let passwordCorrect = false
+  try {
+    passwordCorrect = await bcrypt.compare(req.body.password, rows[0].password)
+  } catch (ex) {}
 
-   if(passwordCorrect){
-        output.success = true;
-      //把登入記錄在SESSION裡
-        req.session.member = {
-          member_id:rows[0].member_id, 
-      }
-      
-     // 包成token
-      output.token= jwt.sign({
-        member_id:rows[0].member_id,
-        email:rows[0].email
-            },process.env.JWT_SECRET)
-            output.memberId=rows[0].member_id;
-            output.memberEmail=rows[0].email
-            res.json(output);
-     }else{
-          output.error="402帳號密碼錯誤"
-          return res.json(output);
-        }
+  if (passwordCorrect) {
+    output.success = true
+    //把登入記錄在SESSION裡
+    req.session.member = {
+      member_id: rows[0].member_id,
+    }
 
-
-
-
-   
-
-} )
-
-
-
-
-
-
-
-
-
-
+    // 包成token
+    output.token = jwt.sign(
+      {
+        member_id: rows[0].member_id,
+        email: rows[0].email,
+      },
+      process.env.JWT_SECRET
+    )
+    output.memberId = rows[0].member_id
+    output.memberEmail = rows[0].email
+    res.json(output)
+  } else {
+    output.error = '402帳號密碼錯誤'
+    return res.json(output)
+  }
+})
 
 app.use('/member_page', require('./routes/member_page'))
 app.use('/article_page', require('./routes/article_page'))
