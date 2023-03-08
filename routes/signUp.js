99e7b122278039137277509b4f6e151d4baae739
olar88 <parkeunyeon18@gmail.com>
2023-03-08 23:31:14 +0800

    申請會員API修正好

--- routes/signUp.js
+++ routes/signUp.js
@@ -1,10 +1,12 @@
 const express = require('express')
 const dataBase = require('./../modules/db_connect')
+const router = express.Router()
 //E-mail格式認證
 const validator = require('email-validator')
 //password格式認證
 const passwordValidator = require('password-validator')
 const passVali = new passwordValidator()
+//密碼規則:最少6最多16含一大寫一小寫兩數字沒空白
 passVali
   .is()
   .min(6)
@@ -20,16 +22,12 @@ passVali
   .not()
   .spaces()
 
-const router = express.Router()
 //密碼加密bcrypt
 const bcrypt = require('bcryptjs')
+//Token
+const jwt = require('jsonwebtoken')
 
-//登入路由
-router.use((req, res, next) => {
-  next()
-})
-
-//申請API
+//申請會員API
 router.post('/', async (req, res) => {
   let output = {
     success: true,
@@ -37,7 +35,7 @@ router.post('/', async (req, res) => {
     error: {},
     data: {},
   }
-  let dataValue = {
+  output.data = {
     name: req.body.name,
     email: req.body.email,
     password: bcrypt.hashSync(req.body.password, 8) || 0,
@@ -49,11 +47,11 @@ router.post('/', async (req, res) => {
     address_rd: req.body.address_rd,
   }
   //檢查各欄位資料
-  if (dataValue.name.length < 1) {
+  if (output.data.name.length < 1) {
     output.success = false
     output.error.name = '請輸入正確姓名'
   }
-  if (dataValue.email.length < 1 || !validator.validate(dataValue.email)) {
+  if (output.data.email.length < 1 || !validator.validate(output.data.email)) {
     output.success = false
     output.error.email = 'email格式不正確'
   }
@@ -67,22 +65,30 @@ router.post('/', async (req, res) => {
 
   if (output.success) {
     const [result] = await dataBase.query(sql, [
-      dataValue.name,
-      dataValue.email,
-      dataValue.password,
-      dataValue.mobile,
-      dataValue.gender,
-      dataValue.birthday,
-      dataValue.address_city,
-      dataValue.address_dist,
-      dataValue.address_rd,
+      output.data.name,
+      output.data.email,
+      output.data.password,
+      output.data.mobile,
+      output.data.gender,
+      output.data.birthday,
+      output.data.address_city,
+      output.data.address_dist,
+      output.data.address_rd,
     ])
     output.affectedRows = result.affectedRows
-
-    res.json([output, dataValue])
+    output.data.sid = result.insertId
+    // 包成token
+    output.token = jwt.sign(
+      {
+        member_id: output.data.sid,
+        member_email: output.data.email,
+      },
+      process.env.JWT_SECRET
+    )
+    res.json(output)
     return
   }
-  res.json([output, dataValue])
+  res.json([output])
 
   //[result] 結果為陣列,皆會塞進第一個值  這邊不用prepare 直接query,在第二個參數塞值
 })
