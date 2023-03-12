2824cde43fd67a4af2a5a366b53c3b6ea07a11c9
hhddkkss <116135735+hhddkkss@users.noreply.github.com>
2023-03-12 16:34:54 +0800

    '更新'

--- routes/member_page.js
+++ routes/member_page.js
@@ -1,29 +1,24 @@
-const express = require('express');
+const express = require('express')
 const dataBase = require('./../modules/db_connect')
 
-const router = express.Router();
+const router = express.Router()
 
 //登入後才能檢視要塞這
-router.use((req,res,next)=>{
-    next()
+router.use((req, res, next) => {
+  next()
 })
 
-const getMemberData = async(req) =>{
-    let rows = []
-    const sql = `SELECT * FROM member_list WHERE member_id = 1`;
-    [rows] = await dataBase.query(sql);
+const getMemberData = async (req) => {
+  let rows = []
+  const sql = `SELECT * FROM member_list WHERE member_id = 1`
+  ;[rows] = await dataBase.query(sql)
 
-    return {rows}
-   
+  return { rows }
 }
 
-
-
-
-router.get('/member_api', async (req,res)=>{
-    res.json(await getMemberData(req))
+router.get('/member_api', async (req, res) => {
+  res.json(await getMemberData(req))
 })
-
 
 //記得!!!!----將路由作為模組打包匯出----
 module.exports = router
