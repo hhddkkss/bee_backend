39721c7cccd0dbcd4e306b410af251c8893d3876
hhddkkss <116135735+hhddkkss@users.noreply.github.com>
2023-06-06 15:07:26 +0800

    修正小問題

--- routes/home_page.js
+++ routes/home_page.js
@@ -9,7 +9,7 @@ router.use((req, res, next) => {
 
 // //首頁拉商品
 const getproductdata = async (req) => {
-  const sql = `SELECT product_id,product_name, product_price, product_pic FROM product_total LIMIT 0,6 `
+  const sql = `SELECT product_id,product_name, product_price, product_pic FROM product_total LIMIT 289,6 `
 
   // const [rows] = await dataBase.query(sql, [
   //   req.body.product_name,
@@ -30,10 +30,32 @@ router.get('/home_product', async (req, res) => {
   res.json(await getproductdata(req))
 })
 
+const recommendProduct = async (req) => {
+  const sql = `SELECT product_id,product_name, product_price, product_pic FROM product_total LIMIT 289,3 `
+
+  // const [rows] = await dataBase.query(sql, [
+  //   req.body.product_name,
+  //   req.body.product_price,
+  //   req.body.product_pic,
+  //   req.params.product_id,
+  // ]);
+
+  const [rows] = await dataBase.query(sql)
+  const home_pic = rows.map((v, i) => {
+    return { ...v, product_pic: v.product_pic.split(',') }
+  })
+  console.log('a00', rows, 'a001', home_pic, 'A03', [home_pic])
+  return home_pic
+}
+
+router.get('/recommend_product', async (req, res) => {
+  res.json(await recommendProduct(req))
+})
+
 // //首頁拉文章
 
 const getarticles = async (req) => {
-  const sql = `SELECT article_id, title, content_1, article_pic_main FROM articles LIMIT 0,3 `
+  const sql = `SELECT article_id, title, content_1, article_pic_main ,created_at FROM articles LIMIT 1,3`
   const [rows] = await dataBase.query(sql, [req.params.id])
   return rows
 }
